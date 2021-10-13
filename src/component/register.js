60d1f0caea5c91d058dadec5f2f546eeadd2f634
bhatiashivam5 <bhatiashivam35@gmail.com>
2021-10-13 17:11:14 +0530

    first commit

--- src/component/register.js
+++ src/component/register.js
@@ -9,7 +9,7 @@ import * as Yup from 'yup';
 const initialValues = {
     firstName: '',
     lastName: '',
-    age:'',
+    password: '',
     email: '',
     contact: '',
     location: ''
@@ -17,7 +17,7 @@ const initialValues = {
 
 const onSubmit = values => {
     console.log('Form data', values);
-    
+
 }
 
 
@@ -25,7 +25,7 @@ const onSubmit = values => {
 const validationSchema = Yup.object({
     firstName: Yup.string().required('Required'),
     lastName: Yup.string().required('Required'),
-    age: Yup.number().required('Required'),
+    password: Yup.string().required('Required'),
     contact: Yup.number().required('Required'),
     email: Yup.string()
         .email('Invalid email format')
@@ -44,15 +44,71 @@ function Register() {
     })
 
 
+    const SubmitData = async (e) => {
+        e.preventDefault();
+        console.log(formik.values);
+        const { firstName, lastName, contact, email, password, location } = formik.values;
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+            ,
+            body: JSON.stringify(formik.values)
+        };
+        fetch('http://localhost:6000/userData', options).then((res) => {
+            res.json();
+            if (res.status === 401 || !res) {
+                window.alert("invalid registration");
+                console.log('invalid registration');
+            }
+            else {
+                 window.alert("successfully registration");
+                 console.log('successfully registration');
+            }
+        })
+            // .catch((
+            //     err
+            // ) =>{
+            //     console.log(err);
+            // }) 
+
+    }
+
+    //    const res= await fetch('http://localhost:6000/userData', {
+    //        method:"POST",
+    //        headers:{
+    //            "Content-Type":"application/json"
+    //        },
+    //        body:JSON.stringify({
+    //         firstName, lastName, contact, email,password, location
+    //        })
+    //    });
+    //    const data = await res.json();
+    //    console.log(data);
+
+    //        if(res.status === 401 || !res){
+    //            window.alert("invalid registration");
+    //            console.log('invalid registration');
+    //        }
+    //        else{
+    //         // window.alert("successfully registration");
+    //         // console.log('successfully registration');
+    //     }
+
+    // }
+
     return (
         <Card sx={{ minWidth: 275 }} className="card">
-            <form onSubmit={formik.handleSubmit}>
+            <form method="POST" onSubmit={formik.handleSubmit}>
                 <CardContent>
                     <Typography sx={{ fontSize: 20, fontWeight: 'bold', color: 'white' }} color="text.secondary" gutterBottom>
                         REGISTRATION FORM
                     </Typography>
                     <label htmlFor='firstName'>FirstName</label>
                     <input type='text' id='firstName' name='firstName' onChange={formik.handleChange} value={formik.values.firstName} />
+
 
                     {formik.touched.firstName && formik.errors.firstName ? (
                         <div className='error'>{formik.errors.firstName}</div>
@@ -65,13 +121,6 @@ function Register() {
                         <div className='error'>{formik.errors.lastName}</div>
                     ) : null}
 
-                    <label htmlFor='age'>Age</label>
-                    <input type='text' id='age' name='age' onChange={formik.handleChange} value={formik.values.age} />
-
-                    {formik.touched.age && formik.errors.age ? (
-                        <div className='error'>{formik.errors.age}</div>
-                    ) : null}
-
                     <label htmlFor='email'>Email</label>
                     <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
 
@@ -79,12 +128,6 @@ function Register() {
                         <div className='error'>{formik.errors.email}</div>
                     ) : null}
 
-                    <label htmlFor='contact'>Contact</label>
-                    <input type='text' id='contact' name='contact' onChange={formik.handleChange} value={formik.values.contact} />
-
-                    {formik.touched.contact && formik.errors.contact ? (
-                        <div className='error'>{formik.errors.contact}</div>
-                    ) : null}
 
                     <label htmlFor='location'>Location</label>
                     <input type='text' id='location' name='location' onChange={formik.handleChange} value={formik.values.location} />
@@ -92,9 +135,27 @@ function Register() {
                     {formik.touched.location && formik.errors.location ? (
                         <div className='error'>{formik.errors.location}</div>
                     ) : null}
+
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' id='password' name='password' onChange={formik.handleChange} value={formik.values.password} />
+
+                    {formik.touched.password && formik.errors.password ? (
+                        <div className='error'>{formik.errors.password}</div>
+                    ) : null}
+
+
+
+                    <label htmlFor='contact'>Contact</label>
+                    <input type='tel' id='contact' name='contact' onChange={formik.handleChange} value={formik.values.contact} />
+
+                    {formik.touched.contact && formik.errors.contact ? (
+                        <div className='error'>{formik.errors.contact}</div>
+                    ) : null}
+
+
                 </CardContent>
 
-                <button>Submit</button>
+                <button onClick={SubmitData} type="submit">Submit</button>
                 <button>Clear</button>
 
             </form>
